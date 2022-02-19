c447c4325c87b0b738b5fc3adddb9fc686293899
Bakyt Adlet <bakyt_adlet@mail.ru>
2022-02-19 22:06:52 +0600

    init project

---
+++ server/src/app.service.ts
@@ -0,0 +1,17 @@
+ import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+
+  getSomething(): string {
+    return 'Something can be sent back';
+  }
+
+}
+
+
+
